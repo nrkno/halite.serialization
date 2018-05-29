f5e318648fc9d22abee0199e4661fde471535020
Steinar Aasheim <steinar.aasheim@nrk.no>
2018-05-29 08:58:42 +0200

    Added support for nullvalue handling when spesified on a property

--- source/Halite.Tests/SnakeResource.cs
+++ source/Halite.Tests/SnakeResource.cs
@@ -1,7 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Halite.Tests
 {
     public class SnakeResource : HalResource<HalLinks>
     {
         public string LongTail { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
     }
 }
